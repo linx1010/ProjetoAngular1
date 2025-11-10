7e51257803b0aca978dfe8b73c562187cdfee1b1
Leandro Oliver <linxfsa@hotmail.com>
2025-11-10 20:30:30 -0300

    add intercptor e combo de função

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing.module';
 
 // Angular Calendar 0.31.1
-
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
@@ -16,19 +15,23 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { FlatpickrModule } from 'angularx-flatpickr';
 
+// ✅ importa a função interceptor
+import { authInterceptorFn } from './app/auth.interceptor';
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     importProvidersFrom(
       BrowserAnimationsModule,
-      HttpClientModule,
       FormsModule,
       NgbModule,
       FlatpickrModule.forRoot(),
       CalendarModule.forRoot({
         provide: DateAdapter,
         useFactory: adapterFactory,
-      }) // Versão 0.31.1 não precisa de adapter
-    )
+      })
+    ),
+    // ✅ registra o interceptor como função
+    provideHttpClient(withInterceptors([authInterceptorFn]))
   ]
 });
